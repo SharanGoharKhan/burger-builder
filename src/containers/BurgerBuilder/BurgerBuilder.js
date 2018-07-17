c4c3106bd48642938835d42e7c6ca7fa5c533b73
Sharan Khan <sharankhan@Sharans-MacBook-Air.local>
2018-07-18 01:05:20 +0500

    fixed bug issues

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,8 +7,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders'
 import Spinner from '../../components/UI/Spinner/Spinner'
-// import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
-import WithErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -144,4 +143,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default withErrorHandler(BurgerBuilder);
+export default withErrorHandler(BurgerBuilder, axios);
