1a790831d9aacabfebf95f0a5679fe21768a31f0
Sharan Khan <sharan.gohar@gmail.com>
2018-08-12 16:20:52 +0500

    added error message for invalid login

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -4,7 +4,7 @@ import * as actions from '../../store/actions/index'
 import Input from '../../components/UI/Input/Input'
 import classes from './Auth.css'
 import Button from '../../components/UI/Button/Button'
-
+import Spinner from '../../components/UI/Spinner/Spinner'
 class Auth extends Component {
     state = {
         controls: {
@@ -98,7 +98,7 @@ class Auth extends Component {
                 config: this.state.controls[key]
             });
         }
-        const form = formElementsArray.map(formElement =>(
+        let form = formElementsArray.map(formElement =>(
             <Input
             key={formElement.id}
             elementType={formElement.config.elementType}
@@ -110,8 +110,19 @@ class Auth extends Component {
             changed={(event) => this.inputChangedHandler(event, formElement.id)}
             />
         ))
+        if (this.props.loading) {
+            form = <Spinner />
+        }
+        let errorMessage = null
+
+        if(this.props.error) {
+            errorMessage = (
+                <p>{this.props.error.message}</p>
+            )
+        }
         return (
             <div className={classes.Auth}>
+                {errorMessage}
                 <form onSubmit={this.submitHandler}>
                 {form}
                 <Button btnType="Success">SUBMIT</Button>
@@ -123,9 +134,15 @@ class Auth extends Component {
         )
     }
 }
+const mapStateToProps = state => {
+    return {
+        loading: state.auth.loading,
+        error: state.auth.error
+    }
+}
 const mapDispatchToProps = dispatch => {
     return {
         onAuth: (email,password, isSignup) => dispatch(actions.auth(email,password, isSignup))
     }
 }
-export default connect(null,mapDispatchToProps)(Auth);
+export default connect(mapStateToProps,mapDispatchToProps)(Auth);
